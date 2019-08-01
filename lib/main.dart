f5346e5d21bb12f2ed30ee68a19d1a6eeecfab84
Your Name <gf_kadi@esi.dz>
2019-08-02 00:21:34 +0100

    add image picker and signaler screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
@@ -48,12 +50,14 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
     _controller.dispose();
     super.dispose();
   }
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
     home :Scaffold(
+      key: _scaffoldKey,
 
       appBar: AppBar(backgroundColor: Colors.deepPurple,),
 
@@ -66,6 +70,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
         // space to fit everything.
+
         child: ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
@@ -82,13 +87,21 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
                accountName: Text("fouad"), accountEmail: Text("+213 669506475")),
 
             ListTile( title: Text('Signaler un accident'),
-                      leading: Icon(Icons.local_hospital),),
+                      leading: Icon(Icons.local_hospital),
+                      onTap: (){ _scaffoldKey.currentState.openEndDrawer();
+                                 Navigator.push(context, MaterialPageRoute(builder: (context) => new signaler()));    },),
             Divider(),
             ListTile( title: Text('Liste des accidents'),
-              leading: Icon(Icons.directions_car),),
+              leading: Icon(Icons.directions_car),
+              onTap: (){
+
+              },
+            ),
             Divider(),
             ListTile( title: Text('Choisir un chemin'),
-              leading: Icon(Icons.location_on),),
+              leading: Icon(Icons.location_on),
+              onTap: (){ _scaffoldKey.currentState.openEndDrawer();
+              Navigator.push(context, MaterialPageRoute(builder: (context) => new Map()));    },),
             Divider(),
             ListTile( title: Text('SOS'),
               leading: Icon(Icons.call),
@@ -165,7 +178,7 @@ class _loginState extends State<login> {
              onPressed:() {
                Navigator.push(
                context,
-               MaterialPageRoute(builder: (context) => MyApp()));},
+               MaterialPageRoute(builder: (context) => new MyApp()));},
            child:
 
          Row(
@@ -207,6 +220,14 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+
+
+  Completer<GoogleMapController> _controller = Completer();
+  
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -221,7 +242,13 @@ class _MapState extends State<Map> {
 
 
 
-           body: Text('hello'),
+           body:GoogleMap(
+             mapType: MapType.hybrid,
+             initialCameraPosition: _kGooglePlex,
+             onMapCreated: (GoogleMapController controller) {
+               _controller.complete(controller);
+             },
+           ),
 
       ),
     );
@@ -230,3 +257,163 @@ class _MapState extends State<Map> {
 
 
 
+//------------------ Signaler un accident
+
+class signaler extends StatefulWidget {
+  @override
+  _signalerState createState() => _signalerState();
+}
+
+class _signalerState extends State<signaler> {
+
+String s="0";
+  File _image;
+
+
+
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+
+      _image = image;
+
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp
+      (
+      home: Scaffold(
+
+        appBar: AppBar(backgroundColor: Colors.deepPurple, leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),),
+        body: ListView(
+
+          children: <Widget>[
+
+
+
+          Column(
+            children: <Widget>[
+              
+              Image.asset('assets/car-crash.png',scale: 2,),
+
+              Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+
+                 Padding(
+                   padding: const EdgeInsets.only(left: 20),
+                   child: Icon(Icons.place ,size: 50,),
+                 ),
+                 Flexible(child:
+                 Padding(
+                   padding: const EdgeInsets.only(left: 20,right: 40,bottom: 10),
+                   child: Container(child: TextField( decoration:  new InputDecoration(
+                       hintText: 'Ex: alger',
+                       labelText: 'lieu',
+
+                   )
+                     ,)),
+                 ),)
+              ],),
+
+
+              Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Icon(Icons.people ,size: 50,),
+                  ),
+                  Flexible(child:
+                  Padding(
+                    padding:const EdgeInsets.only(left: 20,right: 40,bottom: 10),
+                    child: Container(child: TextField( decoration:  new InputDecoration(
+                      hintText: 'Ex: 10',
+                      labelText: 'nombre de victimes',
+
+
+                    ),
+                      keyboardType: TextInputType.number,
+                      )),
+                  ),)
+                ],),
+
+
+              Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Icon(Icons.calendar_today ,size: 50,),
+                  ),
+                  Flexible(child:
+                  Padding(
+                    padding:const EdgeInsets.only(left: 20,right: 40,bottom: 20),
+                    child: Container(child: TextField( decoration:  new InputDecoration(
+                      hintText: 'Ex: ',
+                      labelText: 'Date et heure',
+
+                    )
+                      ,
+                    keyboardType: TextInputType.datetime)),
+                  ),)
+                ],),
+              
+              
+               IconButton( iconSize: 40, color: Colors.deepPurpleAccent, icon: Icon(Icons.add_photo_alternate), onPressed:() {getImage() ;
+
+
+               },),
+
+              _image == null
+                  ? Text('Ajouter une image')
+                  : Row( mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[  Icon( Icons.check,color: Colors.green,), Text("Image chargé"),],),
+
+
+
+              Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  onPressed: () {},
+                  elevation: 10,
+                  color: Colors.deepPurpleAccent,
+                  child: Text("Soummetre",style: TextStyle(color: Colors.white ),),
+
+                ),
+              )
+
+            ],
+          ),
+
+
+
+
+
+
+        ],)
+
+      ),
+
+    );
+  }
+}
